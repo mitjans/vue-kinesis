9c2aaf731ff7a4be41fb68c6518ce1aadf8414cf
Carles Mitjans Coma <mitjans.coma@gmail.com>
2021-05-01 14:09:38 +0200

    feat: Use tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Under construction...</h1>
+  <div class="h-screen w-screen">
+    <div class="text-5xl font-extrabold p-5">
+      Under construction...
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
