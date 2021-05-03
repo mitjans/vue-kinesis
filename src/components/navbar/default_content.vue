12ca5e52e8affa453aac7832a9dbfa736a04fbb7
Carles Mitjans <cmitjans@wiris.com>
2021-05-03 17:10:04 +0200

    chore: Add profile info

--- src/components/navbar/default_content.vue
+++ src/components/navbar/default_content.vue
@@ -4,21 +4,62 @@
       id="profile-image"
       src="@assets/images/profile.jpeg"
     >
+    <div id="profile-info">
+      <h1>Carles Mitjans Coma</h1>
+      <h2>Software Engineer</h2>
+      <div id="email-container">
+        <MailIcon
+          id="mail-icon"
+          size="0.5x"
+        />
+        <a href="mailto:mitjans.coma@gmail.com">
+          mitjans.coma@gmail.com
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { MailIcon } from '@heroicons/vue/solid';
 
+export default {
+  components: {
+    MailIcon,
+  },
 };
 </script>
 
 <style>
 #navbar-default-content {
-  @apply pl-20 pr-20;
+  @apply flex space-x-10 items-center pl-20 pr-20;
 }
 
 #profile-image {
   @apply rounded-full w-80 h-80 shadow-xl;
+}
+
+#profile-info {
+  @apply flex flex-col space-y-2 text-white;
+}
+
+#profile-info h1 {
+  @apply text-5xl font-bold;
+}
+
+#profile-info h2 {
+  @apply text-2xl font-semibold;
+}
+
+#email-container {
+  @apply flex space-x-2;
+}
+
+#mail-icon {
+  @apply inline-flex;
+}
+
+#email-container a {
+  @apply text-xl underline;
 }
 </style>
