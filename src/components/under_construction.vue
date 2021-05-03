d026a1ab8ce6a0b74bcb72a61109b89cc72cc11e
Carles Mitjans <cmitjans@wiris.com>
2021-05-03 11:28:58 +0200

    chore: Update under construction page

--- src/components/under_construction.vue
+++ src/components/under_construction.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="under-construction_container">
-    <h1>Under construction</h1>
-    <img
-      id="under-construction_image"
-      src="@assets/images/under_construction.jpg"
-    >
-    <h2>By Carles Mitjans</h2>
+    <h1>
+      Coming soon
+    </h1>
+    <h2>
+      by Carles Mitjans
+    </h2>
   </div>
 </template>
 
@@ -17,11 +17,15 @@ export default {
 
 <style>
 #under-construction_container {
-  @apply flex flex-col items-center text-center space-y-4;
+  @apply flex flex-col justify-center text-center space-y-4 bg-gray-600 w-full h-full;
 }
 
 #under-construction_container h1 {
-  @apply w-full bg-red-700 text-5xl text-white font-extrabold p-5;
+  @apply w-full text-5xl text-white font-extrabold;
+}
+
+#under-construction_container h2 {
+  @apply w-full text-2xl text-white font-thin;
 }
 
 img {
